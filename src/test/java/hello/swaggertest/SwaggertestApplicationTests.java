c039b97f765b5e5ebe8524ab1fa050588c797f22
minsoo.lee <minsoo.lee@8byte.co.kr>
2021-12-14 18:17:06 +0900

    first commit

---
+++ src/test/java/hello/swaggertest/SwaggertestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.swaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
